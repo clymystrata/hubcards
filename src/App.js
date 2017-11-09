0de8d97aeac24bed980b8b85bd6c25b32fb687ce
rsmellow <dev@rsmellow.com>
2017-11-09 01:52:30 -0500

    stylin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 import './index.css';
 import 'react-web-tabs/dist/react-web-tabs.css';
@@ -26,8 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>The goings on</h2>
         </div>
         <div>
           <CardList cards = {this.state.cards} />
--- src/components/cards.js
+++ src/components/cards.js
@@ -2,16 +2,16 @@
 // start small and embrace refactoring
 // use function components until you can't
 
-import React from 'react';
+import React, {Component} from 'react';
 import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
 
-const Avatar = (props) => {
-  return (
+const Avatar = (props) => 
+  props.isCurrent ?
     <div className="avatar">
         <img src={props.url} />
-    </div>
-  );
-};
+    </div> :
+    <p className="nameTab">{props.name}</p>
+
 
 const RepoInfo = (props) => {
   return (
@@ -26,36 +26,46 @@ const RepoInfo = (props) => {
 
 const UserInfo = (props) => {
   return(
-    <TabPanel tabId={props.id}>
+    <TabPanel tabId={`${props.id}`}>
+    <div classNmae="infoTab">
       <h1 className="nameBadge">{props.name}</h1>
 
       {props.repos ? props.repos.map(ri => <RepoInfo key={ri.id} info={ri} />): <p>No Public Repositories</p>}
+    </div>
     </TabPanel>
   );
 };
 
 const Card = (props) => {
   return (
-      // "javascript" styling (not inline CSS)
-      // can be used to dynamically generate styles.
-      // normal CSS needs to use className instead of class
-      <Tab tabFor={props.id}>
-          <Avatar url={props.avatar_url} />
+      <Tab tabFor={`${props.id}`}>
+      <div className="avTab">
+          <Avatar url={props.avatar_url} 
+                  cardId={props.id}
+                  isCurrent={props.id == props.curId}
+                  name={props.name}
+          />
+      </div>
       </Tab>
     );
 };
 
-const CardList = (props) => {
-  const defaultCard = props.cards.length > 0 ? props.cards[0].id : null;
-	return (
-  	<Tabs defaultTab={defaultCard} vertical>
-      <TabList>
-    	{props.cards.map(card => <Card key={card.id} {...card} />)}
-      </TabList>
-      {props.cards.map(card => <UserInfo key={card.id} {...card} />)}
-    </Tabs>
-  );
-}
+class CardList extends Component {
+  state = {}
+  
+  ChangeCard = (cardId) => {
+    this.setState({curCardId: cardId});
+  }
+  render() {
+    return(
+        <Tabs vertical onChange = {(id) => this.ChangeCard(id)}>
+          <TabList>
+          {this.props.cards.map(card => <Card curId={this.state.curCardId} key={card.id} {...card} />)}
+          </TabList>
+          {this.props.cards.map(card => <UserInfo key={card.id} {...card} />)}
+        </Tabs>
+    )}
+};
 
 export default CardList;
 
